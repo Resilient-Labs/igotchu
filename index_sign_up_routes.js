00952e2673641c5d2b874e1f232d63b4e74a67f4
sebastian-vivas <seb.vivas.dev@gmail.com>
2020-05-19 14:43:25 -0400

    Sign Up route ticket

---
+++ index_sign_up_routes.js
@@ -0,0 +1,21 @@
+module.exports = function(app, passport, db) {
+
+// normal routes ===============================================================
+
+    // show the home page (will also have our login links)
+    app.get('/', function(req, res) {
+        res.render('index.ejs');
+    });
+
+        // SIGNUP =================================
+        // show the signup form
+        app.get('/signup', function(req, res) {
+            res.render('signup.ejs', { message: req.flash('signupMessage') });
+        });
+
+        // process the signup form
+        app.post('/signup', passport.authenticate('local-signup', {
+            successRedirect : '/profile', // redirect to the secure profile section
+            failureRedirect : '/signup', // redirect back to the signup page if there is an error
+            failureFlash : true // allow flash messages
+        }));
