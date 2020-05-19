f80dda6e69e2e694377e3efc4371c807239ad7b4
aiperieldar <aiperieldar.dev@gmail.com>
2020-05-19 16:42:56 -0400

    creating beIssue12_Aiperi branch

--- routes.js
+++ routes.js
@@ -1,18 +1,9 @@
-
-app.get('/sold', isLoggedIn, function(req, res) {
-  db.collection('postedItem').find().toArray((err, result) => {
+app.get('/profile', isLoggedIn, function(req, res) {
+  db.collection('users').find().toArray((err, result) => {
     if (err) return console.log(err)
-    res.render('soldItems.ejs', {
-      postedItem: 'sold'
-    })
-  })
-});
-
-app.get('/bought', isLoggedIn, function(req, res) {
-  db.collection('postedItem').find().toArray((err, result) => {
-    if (err) return console.log(err)
-    res.render('boughtItems.ejs', {
-      postedItem: 'bought'
+    res.render('profile.ejs', {
+      user: req.user,
+      postedItem: result
     })
   })
 });
