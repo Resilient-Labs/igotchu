9beb8ff87c26b1f7bf1ab6909c63bd375d2ae076
el-roc <eric.eespinoza@gmail.com>
2020-05-20 14:27:29 -0400

    moving Sam's source files to client src folder

--- client /src/index.js
+++ client /src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './css/index.css';
+import Landing_App from './Landing_App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Landing_App />
   </React.StrictMode>,
   document.getElementById('root')
 );
--- src/App.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import Landing_App from './Landing_App';
-
-test('renders learn react link', () => {
-  const { getByText } = render(<Landing_App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
