707a8d8952b6b04a03d9a4e5f2fb3989074b58c1
Kenneth Cruz <kenneth.s.cruz@gmail.com>
2020-05-20 16:41:24 -0400

    merging sebastian signup routes.js

--- routes.js
+++ routes.js
@@ -1,5 +1,20 @@
-
-
+module.exports = function(app, passport, db) {
+// normal routes ===============================================================
+    // show the home page (will also have our login links)
+    app.get('/', function(req, res) {
+        res.render('index.ejs');
+    });
+        // SIGNUP =================================
+        // show the signup form
+        app.get('/signup', function(req, res) {
+            res.render('signup.ejs', { message: req.flash('signupMessage') });
+        });
+        // process the signup form
+        app.post('/signup', passport.authenticate('local-signup', {
+            successRedirect : '/profile', // redirect to the secure profile section
+            failureRedirect : '/signup', // redirect back to the signup page if there is an error
+            failureFlash : true // allow flash messages
+        }));
 
 // user is login and stores data into the database
 app.get('/createItemPage', isLoggedIn, function(req, res) {
@@ -21,3 +36,5 @@ app.get('/createItemPage', isLoggedIn, function(req, res) {
       console.log('saved to database')
       res.redirect('/createItemPage')
     })
+  }
+};
