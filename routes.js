a23bd7e55fca4d07fb4b9743f3ca7ef3f4731dfc
Kenneth Cruz <kenneth.s.cruz@gmail.com>
2020-05-20 16:50:29 -0400

    merging abbey's userID to routes.js

--- routes.js
+++ routes.js
@@ -1,5 +1,5 @@
 module.exports = function(app, passport, db) {
-// normal routes ===============================================================
+// normal routes (Sebastian) ===============================================================
     // show the home page (will also have our login links)
     app.get('/', function(req, res) {
         res.render('index.ejs');
@@ -16,7 +16,22 @@ module.exports = function(app, passport, db) {
             failureFlash : true // allow flash messages
         }));
 
-// user is login and stores data into the database
+// (abbey, get user ID)
+        app.get("/user/:id", function(req, res) {
+          let userId = ObjectId(req.params.zebra)
+          // the let userID attain the unique userID number in order to get the specfic profile
+          db.collection("postedItem").find({_id: userId , itemType: itemsSold}).toArray((err, result) => {
+            // this will go to the postedItem database, find the user and only what has been sold, not sold AND BOUGHT
+            if (err) return console.log(err)
+            res.render("sellerPage.ejs", {
+              items: result,
+              users : req.user,
+        // show you the user is selling
+            })
+          })
+        });
+
+// user is login and stores data into the database (Victor)
 app.get('/createItemPage', isLoggedIn, function(req, res) {
   console.log(req)
     db.collection('itemPosts').find().toArray((err, result) => {
@@ -28,7 +43,7 @@ app.get('/createItemPage', isLoggedIn, function(req, res) {
     })
 });
 
-  //Create Post =========================================================================
+  //Create Post (Victor) =========================================================================
   app.post('/postItem', upload.single('file-to-upload'), (req, res, next) => {
     let uId = ObjectId(req.session.passport.user)
     db.collection('itemPosts').save({posterId: uId, description: req.body.description, status: req.body.status, price: req.body.price imgPath: 'images/uploads/' + req.file.filename}, (err, result) => {
