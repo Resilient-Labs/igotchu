08612359dfee7fc68d400cd8017704810cf92cf0
el-roc <eric.eespinoza@gmail.com>
2020-05-21 16:56:15 -0400

    updating master branch for heroku-build deployment

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = process.env.PORT || 5000;
+
+////
+app.use(express.static(path.join(__dirname, 'react-ui/build')));
 
 // console.log that your server is up and running
 app.listen(port, () => console.log(`Listening on port ${port}`));
@@ -9,4 +13,9 @@ app.listen(port, () => console.log(`Listening on port ${port}`));
 // connect express_backend 
 app.get('/express_backend', (req, res) => {
   res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
+});
+
+////////
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/react-ui/build/index.html'));
 });
